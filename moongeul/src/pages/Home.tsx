a32d1b25c30a6ac7104cda26922421ecdcb0ad70
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-08-19 01:18:05 +0900

    chore: ionic, tailwindCSS 환경설정

---
+++ moongeul/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage className={"bg-black"}>
+      <h1 className={"text-white"}>hi</h1>
+    </IonPage>
+  );
+};
+
+export default Home;
