d42f14dc57d310fe521f47ba6581e42eee089058
유림 <yulimh001@gmail.com>
2025-01-24 01:51:21 +0900

    feat: mypage content 기능 추가

--- moongeul/src/pages/Mypage.tsx
+++ moongeul/src/pages/Mypage.tsx
@@ -1,10 +1,61 @@
-import {IonPage} from "@ionic/react";
+import {useState} from "react";
+import {IonContent, IonPage} from "@ionic/react";
+
 import Navbar from "../components/common/Navbar";
+import Header from "../components/common/Header";
+import MyPageHeader from "../components/mypage/MyPageHeader";
+import MyPageContent from "../components/mypage/MyPageContent";
+import Modal from "../components/common/Modal";
+import Button from "../components/common/Button";
 
 const Mypage = () => {
+    const [isOpenLogoutModal, setIsOpenLogoutModal] = useState(true);
+
+    const logoutModalContent = () => {
+        return (
+            <div className={"px-1 py-4"}>
+                <div className={"text-white title1"}>
+                    로그아웃 하시겠습니까?
+                </div>
+            </div>
+        )
+    }
+
+    const logoutModalBottomButton = () => {
+        return (
+            <div className={"flex gap-x-3"}>
+                <Button
+                    onClick={() => {
+                        setIsOpenLogoutModal(false);
+                    }}
+                    className={"border-lightGray-text-darkGray-bottom-button"}>
+                    아니요
+                </Button>
+                <Button
+                    onClick={() => {
+                        setIsOpenLogoutModal(false);
+                    }}
+                    className={"brand-bottom-button"}>
+                    예
+                </Button>
+            </div>
+        )
+    }
+
     return (
-        <IonPage>
-            <Navbar />
+        <IonPage className="page-mypage">
+            <IonContent fullscreen>
+                {isOpenLogoutModal &&
+                    <Modal
+                        content={logoutModalContent}
+                        bottomButton={logoutModalBottomButton}
+                        setClose={setIsOpenLogoutModal}/>}
+                <Header/>
+                <MyPageHeader/>
+                <div className={"h-[284px]"} />
+                <MyPageContent setIsOpenLogoutModal={setIsOpenLogoutModal}/>
+                <Navbar/>
+            </IonContent>
         </IonPage>
     )
 }
---
+++ moongeul/src/utils/mypage.ts
@@ -0,0 +1,7 @@
+export const myPageContents = [
+    {content: "내가 쓴 글", path: "/mypage/my-post"},
+    {content: "내가 쓴 댓글", path: "/mypage/my-comment"},
+    {content: "좋아요 누른 게시글", path: "/mypage/like-post"},
+    {content: "인용된 글", path: "/mypage/quoted-post"},
+    {content: "정보 공개 여부 설정", path: "/mypage/privacy-setting"},
+]
