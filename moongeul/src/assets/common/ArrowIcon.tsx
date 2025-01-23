b43a2b6c7744b6e78ab57a81b60649279cfd6c79
유림 <yulimh001@gmail.com>
2025-01-24 01:50:24 +0900

    style: ArrowIcon 재활용가능하도록 코드 변경

--- moongeul/src/assets/common/ArrowIcon.tsx
+++ moongeul/src/assets/common/ArrowIcon.tsx
@@ -3,22 +3,33 @@ import type { SVGProps } from "react";
 
 interface BackIconProps extends SVGProps<SVGSVGElement> {
     direction?: "left" | "right"; // 방향을 정의
+    size?: number; // 크기 (비율을 유지하기 위해 width와 height를 동시에 설정)
+    color?: string; // 경로의 색상
+    strokeWidth?: number; // 선 두께
 }
 
-const ArrowIcon = ({ direction = "left", ...props }: BackIconProps) => {
+const ArrowIcon = ({
+                       direction = "left",
+                       size = 20, // 기본 크기
+                       color = "#000", // 기본값
+                       strokeWidth = 1,
+                       ...props
+
+    }: BackIconProps) => {
     // 방향에 따라 회전값 설정
     const rotation = direction === "right" ? "rotate(180)" : "rotate(0)";
 
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width={11}
-            height={20}
+            width={size}
+            height={size * (20 / 10)}
+            viewBox="0 0 10 20" // 원본 크기 설정
             fill="none"
             transform={rotation} // 방향에 따라 회전 적용
             {...props}
         >
-            <path stroke="#000" d="m10.5 1-9 9 9 9" />
+            <path stroke={color} strokeWidth={strokeWidth} d="m10.5 1-9 9 9 9" />
         </svg>
     );
 };
